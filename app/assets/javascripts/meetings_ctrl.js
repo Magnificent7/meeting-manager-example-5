5f73a11a7fd94798f406ca5821b078e827e7d3c7
dani <dani.zaghian@gmail.com>
2017-06-25 11:41:15 -0700

    added angular view

---
+++ app/assets/javascripts/meetings_ctrl.js
@@ -0,0 +1,13 @@
+/* global angular */
+(function(){
+  angular.module("app").controller("meetingsCtrl", function($scope, $http){
+    
+    $scope.setup = function () {
+      $http.get("/api/v1/meetings.json").then(function(response){
+        console.log(response.data);
+        $scope.meetings = response.data;
+      });
+    };
+
+  });
+}());
