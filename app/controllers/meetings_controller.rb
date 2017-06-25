ff6c2bbcf4c48699286f9a8e98ee48f9fc252d6a
dani <dani.zaghian@gmail.com>
2017-06-25 10:00:26 -0700

    added new and create actions

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -12,4 +12,42 @@ class MeetingsController < ApplicationController
     @meeting = Meeting.find_by(id: params[:id])
     render "show.html.erb"
   end
+
+  def new
+    @meeting = Meeting.new
+    @tags = Tag.all
+  end
+
+  def create
+    @meeting = Meeting.new(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      notes: params[:notes],
+      )
+    if @meeting.save
+      params[:tags].each do |tag_id|
+        MeetingTag.create(
+          meeting_id: @meeting.id,
+          tag_id: tag_id
+        )
+      end
+      flash[:success] = "Meeting Successfully Created!"
+      redirect_to '/'
+    else
+      @tags = Tag.all
+      render :new
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
