ae3d70a21ad27d7e70e302fea6836dc386f1fa34
dani <dani.zaghian@gmail.com>
2017-06-25 10:48:57 -0700

    added form partial and tags updating

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -56,6 +56,13 @@ class MeetingsController < ApplicationController
       end_time: params[:end_time],
       notes: params[:notes],
       )
+      @meeting.tags.destroy_all
+      params[:tags].each do |tag_id|
+        MeetingTag.create(
+          meeting_id: @meeting.id,
+          tag_id: tag_id
+        )
+      end
       flash[:success] = "Meeting Successfully Updated!"
       redirect_to meeting_path(@meeting)
     else
