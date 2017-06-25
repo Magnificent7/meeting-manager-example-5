b241cae8e6bbceea2e7d6ea5beba1909e384f6c3
dani <dani.zaghian@gmail.com>
2017-06-25 13:53:55 -0700

    added angular post for new meeting

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -4,10 +4,66 @@
     
     $scope.setup = function () {
       $http.get("/api/v1/meetings.json").then(function(response){
-        console.log(response.data);
         $scope.meetings = response.data;
+      });
+      $http.get("/api/v1/tags.json").then(function(response){
+        $scope.tags = response.data;
+      });
+    };
+
+    $scope.orderByAttribute = function(attribute) {
+      if(attribute !== $scope.orderAttribute) {
+        $scope.descending = false;
+      } else {
+        $scope.descending = !$scope.descending;
+      }
+      $scope.orderAttribute = attribute;
+    };
+
+    $scope.filterByTag = function(tag) {
+      $scope.tagFilterId = tag.id;
+      console.log($scope.tagFilterId);
+    };
+
+    $scope.matchTag = function(meeting) {
+      if($scope.tagFilterId) {
+        // use a for loop to see if $scope.tagFilterId exists in meeting.tags
+        for (var i =0; i < meeting.tags.length; i++){
+          var currentTag = meeting.tags[i];
+          if(currentTag.id == $scope.tagFilterId) {
+            return true;
+          }
+        }
+        return false;
+      } else
+      return true;
+    };
+
+    $scope.createMeeting = function(name, address, startTime, endTime, notes, tags) {
+      var newMeeting = {
+        name: name,
+        address: address,
+        start_time: startTime,
+        end_time: endTime,
+        notes: notes,
+        tags: tags
+      };
+      $http.post("/api/v1/meetings.json", newMeeting).then(function(response){
+        console.log(response);
+        $scope.meetings.push(response.data);
+      }, function(error){
+        console.log(error);
       });
     };
 
   });
 }());
+
+
+
+
+
+
+
+
+
--- app/controllers/api/v1/meetings_controller.rb
+++ app/controllers/api/v1/meetings_controller.rb
@@ -3,4 +3,30 @@ class Api::V1::MeetingsController < ApplicationController
     @meetings = Meeting.all
     render 'index.json.jbuilder'
   end
+
+  def create
+    @meeting = Meeting.new(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      notes: params[:notes],
+      )
+    if @meeting.save
+      params[:tags].each do |tag_id|
+        MeetingTag.create(
+          meeting_id: @meeting.id,
+          tag_id: tag_id
+        )
+      end
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @meeting.errors.full_message}, status: 422
+    end
+  end
+
+  def show
+    @meeting = Meeting.find(params[:id])
+    render 'show.json.jbuilder'
+  end
 end
